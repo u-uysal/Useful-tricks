eaebb56c219045bd881339a26afce395b1a7c669
ufuk <ufuk.uysl5@gmail.com>
2020-06-09 22:38:43 +0200

    add nullish operators

---
+++ nullish.js
@@ -0,0 +1,6 @@
+let firstName = null;
+let lastName = null;
+let nickName = "Supercoder";
+
+// show the first not-null/undefined value
+alert(firstName ? ? lastName ? ? nickName ? ? "Anonymous"); // Supercoder
