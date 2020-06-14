2a1e42dd3956b309b1cd0ad435d5ab62c2d08903
ufuk <ufuk.uysl5@gmail.com>
2020-06-14 22:07:36 +0200

    Add youtube link

--- random.js
+++ random.js
@@ -1,9 +1,10 @@
 let y = Math.round(Math.random() * 100);
-console.log(y)
-
+console.log(y);
 
 // (0-1) *100 = 100*0-100*1 =0-100
 
 function random(min, max) {
-    return min + Math.random() * (max - min);
+  return min + Math.random() * (max - min);
 }
+
+//https://youtu.be/F7toPbnjyA0
