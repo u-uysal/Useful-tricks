f3cf7c1c99d54c049248f481c5c6e726a8ce6006
ufuk <ufuk.uysl5@gmail.com>
2020-06-17 18:26:05 +0200

    includes feature of Es6

---
+++ includes.js
@@ -0,0 +1,11 @@
+let numArray = [1, 2, 3, 4, 5];
+
+console.log(numArray.indexOf(0)) // -1 It means it has no this value
+console.log(numArray.indexOf(2)) // 1
+
+
+
+// includes
+
+console.log(numArray.includes(0)) // false
+console.log(numArray.indexOf(2)) // true
