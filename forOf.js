d5de7b7866b297b28b2dfa9bf6098f6aa6c1b6de
ufuk <ufuk.uysl5@gmail.com>
2020-06-17 16:14:21 +0200

    immutable for - of loop

--- forOf.js
+++ forOf.js
@@ -16,3 +16,12 @@ let fullName = "Dylan Coding God Israel";
 for (const char of fullName) {
     console.log(char);
 }
+
+
+let incomes = [62000, 67000, 75000];
+
+for (let income of incomes) {
+    income += 5000; // you cant set new value by using for - of loop
+}
+
+console.log(incomes);
