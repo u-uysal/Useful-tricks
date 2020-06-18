83ba278a283fc79eb656fac50721a9e4100da1d2
ufuk <ufuk.uysl5@gmail.com>
2020-06-18 18:05:53 +0200

    Define a function in class

--- object.js
+++ object.js
@@ -4,7 +4,9 @@ class Animal {
         this.legs = legs;
     }
 
-
+    makeNoise(sound = 'Loud Noise') { // you can define function in class
+        console.log(sound);
+    }
 }
 
 let cat = new Animal('Cat', 4);
@@ -14,3 +16,5 @@ console.log(cat.legs) // 4
 cat.legs = 3; // reassign
 
 console.log(cat.legs) // 3
+
+cat.makeNoise("meow") // meow
