c56bb174a250015cd558138d1c60e9bdd9598a6f
ufuk <ufuk.uysl5@gmail.com>
2020-06-19 12:23:54 +0200

    Add example

--- arrayDestructing.js
+++ arrayDestructing.js
@@ -6,3 +6,15 @@ let [firstName, middleName, lastName] = ['Dylan', 'Coding God', 'Israel']; // ar
 
 const [z, x, , y] = [1, 2, 3, 4, 5, 6];
 console.log(z, x, y); // 1,2,4
+
+
+
+
+let a = 8,
+    b = 6;
+(() => {
+    "use strict";
+    [a, b] = [b, a]
+})();
+console.log(a);
+console.log(b);
