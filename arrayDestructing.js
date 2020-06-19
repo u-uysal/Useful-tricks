501660b5ff1b0c1a50601f094c13acc171c501be
ufuk <ufuk.uysl5@gmail.com>
2020-06-19 12:23:27 +0200

    Add example

--- arrayDestructing.js
+++ arrayDestructing.js
@@ -1,3 +1,8 @@
 let names = ['Dylan', 'Coding God', 'Israel'];
 
 let [firstName, middleName, lastName] = ['Dylan', 'Coding God', 'Israel']; // array destructing
+
+
+
+const [z, x, , y] = [1, 2, 3, 4, 5, 6];
+console.log(z, x, y); // 1,2,4
