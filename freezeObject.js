4b7d2f9744d7cb5005338f78f9b0bdffed64218b
ufuk <ufuk.uysl5@gmail.com>
2020-06-19 09:02:04 +0200

    Object Freezing

---
+++ freezeObject.js
@@ -0,0 +1,19 @@
+function freezeObj() {
+    "use strict";
+    const MATH_CONSTANTS = {
+        PI: 3.14
+    };
+
+    Object.freeze(MATH_CONSTANTS);
+
+    try {
+        MATH_CONSTANTS.PI = 99;
+    } catch (ex) {
+        console.log(ex);
+    }
+    return MATH_CONSTANTS.PI;
+}
+
+const PI = freezeObj();
+
+console.log(PI); // TypeError: Cannot assign to read only property 'PI' of object '#<Object>' 
